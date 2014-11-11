ace3b267a83f8b2bce7d87131da9c1b16641bb90
William Breathitt Gray <vilhelm.gray@gmail.com>
2014-11-11 10:00:03 -0500

    Export erasePage prototype

--- src/libinterfic.h
+++ src/libinterfic.h
@@ -43,6 +43,7 @@ struct fic_page{
 };
 
 extern unsigned addPaddingPages(FILE *const fp, struct free_page *free_pages, unsigned long *const total_pages, const unsigned long NUM_PAD_PAGES);
+extern unsigned erasePage(FILE *const fp, const unsigned long PAGE_NUM, struct free_page **const free_pages);
 extern unsigned readPage(FILE *const fp, const unsigned long PAGE_NUM, struct fic_page *read_page);
 extern unsigned writeFicHeader(FILE *fp);
 extern unsigned writePage(FILE *const fp, const unsigned long PAGE_NUM, const struct fic_page *const NEW_PAGE, struct free_page **const free_pages, unsigned long *const total_pages);
